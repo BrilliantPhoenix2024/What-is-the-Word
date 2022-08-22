15a1292bc3e319748de4bcd56aec5fccdd2673eb
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-22 23:55:18 +0330

    Create 'def get_input_from_list()' to check the User input

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-list_of_words = ['moon', 'star', 'sun', 'earth', 'mars', 'hello', 'hi', 'red', 'yellow', 'computer']
-
 def get_input():
     while True:
         user_input = input('Enter your guess: ')
         if user_input.isalpha():
             return user_input
-        else:
-            print('Your input is not corect please enter again.')
+        print('Your input is not corect please enter again.')
+    
+        
+def get_input_from_list(words):
+    user_input = get_input()
+        
+    while user_input.lower() not in words:
+        print('Please enter a word from the given list.')
+        user_input = get_input()
+    
+    return user_input.lower()    
         
 def game_help():
     print('-'*10)
@@ -15,20 +22,25 @@ def game_help():
     print('Please start to guess.')
     print('-'*10)
 
-def run_game(number_of_rounds):
+
+def run_game(number_of_rounds, words):
     print(game_help())
-    print(f'You can guess {number_of_rounds} times.')
-    corect_word = list_of_words[8]
+    print(f'Number of guess: {number_of_rounds}')
+    corect_word = words[8]
     
     for i in range(number_of_rounds):
-        user_input = get_input()
-        
+        user_input = get_input_from_list(words)
+       
         if user_input == corect_word:
             print('You Won!')
-            break
+            return
         else:
             print('Your guess is Wrong.')
             print(f'Please try again.number of rounds left: {number_of_rounds-1 - i}')
-            
-run_game(5)
+    print('You Lost!')   
+   
+    
+list_of_words = ['moon', 'star', 'sun', 'earth', 'mars', 'hello', 'hi', 'red', 'yellow', 'computer']        
+
+run_game(5, list_of_words)
        
