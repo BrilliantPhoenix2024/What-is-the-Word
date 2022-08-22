b38add639b17d04b38e93f6bd5c6529ac3349aa3
BrilliantPhoenix2024 <ATEFEH.MOHAMMADI.DINANI@OUTLOOK.COM>
2022-08-23 00:14:39 +0330

    Modify the code to 'import random' choice to the game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 def get_input():
     while True:
         user_input = input('Enter your guess: ')
@@ -5,7 +7,6 @@ def get_input():
             return user_input
         print('Your input is not corect please enter again.')
     
-        
 def get_input_from_list(words):
     user_input = get_input()
         
@@ -14,7 +15,7 @@ def get_input_from_list(words):
         user_input = get_input()
     
     return user_input.lower()    
-        
+          
 def game_help():
     print('-'*10)
     print('Wellcome to the Gussing Game.')
@@ -22,11 +23,10 @@ def game_help():
     print('Please start to guess.')
     print('-'*10)
 
-
 def run_game(number_of_rounds, words):
     print(game_help())
     print(f'Number of guess: {number_of_rounds}')
-    corect_word = words[8]
+    corect_word = random.choice(words)
     
     for i in range(number_of_rounds):
         user_input = get_input_from_list(words)
@@ -37,10 +37,10 @@ def run_game(number_of_rounds, words):
         else:
             print('Your guess is Wrong.')
             print(f'Please try again.number of rounds left: {number_of_rounds-1 - i}')
-    print('You Lost!')   
-   
-    
-list_of_words = ['moon', 'star', 'sun', 'earth', 'mars', 'hello', 'hi', 'red', 'yellow', 'computer']        
+    print('You Lost!')
+    print(f'AI choice: {corect_word}')   
+      
+list_of_words = 'moon star sun earth mars hello hi red yellow computer'.split()       
 
 run_game(5, list_of_words)
        
